1cc7db1bc3967d258686ccadedf1fe33bed53ca6
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-24 09:12:53 -0700

    added git

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,6 @@
 node {
     stage("Say Hello"){
-        properties([parameters([string(defaultValue: 'Mustafa', description: 'paste your name', name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * *')])])
-        sh "echo Hello ${NAME}"
+        git 'https://github.com/mcalik77/jenkins.git'
         
 
     }
