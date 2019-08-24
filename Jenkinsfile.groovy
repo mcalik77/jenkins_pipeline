7e621381ea8631a74c94004154ab8a842d1647bf
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-24 09:03:58 -0700

    ADDED

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,8 @@
 node {
     stage("Say Hello"){
         properties([pipelineTriggers([cron('* * * * *')])])
-        sh "echo Hello ${NAME}",
-        sh "do something"
+        sh "echo Hello ${NAME}"
+        
 
     }
 }
