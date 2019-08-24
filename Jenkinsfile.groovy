28f4fb6d55fb64f4b11f51a14ffbb1e5329eef08
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-24 09:24:25 -0700

    edit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,6 @@
 node {
     stage("Say Hello"){
+        properties([parameters([string(defaultValue: '18.212.102.68', description: '''Dev: 18.212.102.68 QA: 54.89.147.203 Prod: 52.72.201.180''', name: 'Remote_instances', trim: true)])])
         git 'https://github.com/mcalik77/jenkins.git'
         
 
